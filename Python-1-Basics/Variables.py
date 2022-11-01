ea7db2f4dffb6fabf8bb9deaedc64959d1fcd934
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-01 12:19:38 +0700

    add : variables

---
+++ Python-1-Basics/Variables.py
@@ -0,0 +1,26 @@
+result = 10 + 5
+print(result)
+
+result2 = result / 2
+
+test = 'test'
+# print(test)
+
+# test_123123_LOL = 'Variable'
+#2test = 'Wrong Variable'
+
+test_Variable_practise = 'test'
+test_2 = 'test2'
+
+some_words = 'some words'
+
+num1 = 10
+# num1 = num1 + 10
+num1 += 10
+print(num1)
+
+# Exercise --> Create a variable with any valu, increase it by 20 and then print it.
+
+exercise_num = 10
+exercise_num -= 20
+print(exercise_num)
