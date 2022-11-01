97b4b9ea91078d64466022d7443ef9df5dd72f9a
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-01 12:29:37 +0700

    add : functions

---
+++ Python-1-Basics/Functions.py
@@ -0,0 +1,14 @@
+print('test')
+word_length = len('another words')
+print(word_length)
+
+print(abs(-100))
+
+print('one' , 'two' , 'three')
+
+# Exercise --> Max Function
+
+max_value = max(10,5,2,3,1,7)
+# max_value = max(1,7)
+# max_value = max(-1,-2)
+print(max_value)
