474c2e48e614c63683a050b4a7ca6af3a67638c1
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-01 11:57:30 +0700

    add : math-operations

---
+++ Python-1-Basics/Math-operations.py
@@ -0,0 +1,18 @@
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+
+print(10 ** 2) # Power
+print(10 // 3) # Round
+print(7 % 2) # Remainder
+
+print(5 + 5 * 2) 
+print((5 + 5) * 2)
+
+print(10 < 5)
+print(1.1 + 5)
+
+# Exercise --> Get the average of the numbers from 1 to 7
+
+print((1 + 2 + 3 + 4 + 5 + 6 + 7) / 7)
