61661238111ae64808b989973386c5872dcb5391
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-01 11:47:10 +0700

    add : how-code-is-executed

---
+++ Python-1-Basics/How-Code-is-executed.py
@@ -0,0 +1,13 @@
+print('First Line of Code')
+print('Second Line of Code')
+print(1234)
+
+# Exercise -> Print Three
+
+print('   * * *   ')
+print('  * * * *  ')
+print('   * * *   ')
+print('     *     ')
+print('     *     ')
+print('     *     ')
+print('     *     ')
